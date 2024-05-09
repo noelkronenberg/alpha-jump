eb094fc18b24214c576b2e9334ce94dec708c163
Noel Kronenberg <79874249+noelkronenberg@users.noreply.github.com>
2024-05-09 17:13:25 +0200

    added mid game

--- jump-sturdy-ai/src/main/java/benchmark/MoveGeneratorBM.java
+++ jump-sturdy-ai/src/main/java/benchmark/MoveGeneratorBM.java
@@ -106,6 +106,10 @@ public class MoveGeneratorBM {
         MoveGeneratorBM.wrapperBM("b0b0b0b0b0b0/1b0b0b0b0b0b01/8/8/8/8/1r0r0r0r0r0r01/r0r0r0r0r0r0 b");
 
         System.out.println();
+        System.out.println("Mid game: ");
+        MoveGeneratorBM.wrapperBM("2bbbb1b0/1b06/1b01b04/4b03/4r03/3r02b01/1r0r02rr2/2rr2r0 b");
+
+        System.out.println();
         System.out.println("End game: ");
         MoveGeneratorBM.wrapperBM("5b0/1bbb0b0brb0b01/8/3b0r03/8/4b03/1rr1b0r0rrrr1/1r04 b");
 
