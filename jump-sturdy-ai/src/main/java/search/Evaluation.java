37ad25a50dba705731621832ad4b46703f769e39
Noel Kronenberg <79874249+noelkronenberg@users.noreply.github.com>
2024-05-15 11:08:43 +0200

    implemented basic move ordering

--- jump-sturdy-ai/src/main/java/search/Evaluation.java
+++ jump-sturdy-ai/src/main/java/search/Evaluation.java
@@ -1,6 +1,9 @@
 package search;
 
 import game.Color;
+import game.MoveGenerator;
+
+import java.util.*;
 
 public class Evaluation {
 
@@ -51,5 +54,67 @@ public class Evaluation {
         int result = 0;
         result = ratePosition(endBoard, player) - ratePosition(startBoard, player);
         return result;
+    }
+
+    // START: move ordering
+
+    public static LinkedList<Integer> convertMovesToList(LinkedHashMap<Integer, List<Integer>> moves) {
+        LinkedList<Integer> movesList = new LinkedList<>();
+
+        for (int startPosition : moves.keySet()) {
+            List<Integer> possibleEndPositions = moves.get(startPosition);
+            for (int endPosition : possibleEndPositions) {
+                int fullMove = startPosition * 100 + endPosition;
+                movesList.add(fullMove);
+            }
+        }
+
+        return movesList;
+    }
+
+    public static LinkedHashMap<Integer, List<Integer>> convertMovesToMap(List<Integer> movesList) {
+        LinkedHashMap<Integer, List<Integer>> movesMap = new LinkedHashMap<>();
+
+        for (int fullMove : movesList) {
+            int startPosition = fullMove / 100;
+            int endPosition = fullMove % 100;
+
+            if (!movesMap.containsKey(startPosition)) {
+                movesMap.put(startPosition, new ArrayList<>());
+            }
+            movesMap.get(startPosition).add(endPosition);
+        }
+
+        return movesMap;
+    }
+
+    public static void orderMoves(LinkedList<Integer> moves, Color color) {
+        if (color == Color.RED) {
+            moves.sort(Comparator.comparingInt(move -> (move % 100) % 10));
+        } else {
+            moves.sort(Comparator.comparingInt(move -> ((int) move % 100) % 10).reversed());
+        }
+    }
+
+    // END: move ordering
+
+    public static void main(String[] args) {
+        MoveGenerator moveGenerator = new MoveGenerator();
+        LinkedHashMap<Integer, List<Integer>> moves = moveGenerator.getMovesWrapper("5b0/1bbb0b0brb0b01/8/3b0r03/8/4b03/1rr1b0r0rrrr1/1r04 b");
+        System.out.println(moves);
+
+        System.out.println();
+        System.out.println("Moves as list: ");
+        LinkedList<Integer> movesList = convertMovesToList(moves);
+        System.out.println(movesList);
+
+        System.out.println();
+        System.out.println("Sorted moves: ");
+        orderMoves(movesList, Color.RED);
+        System.out.println(movesList);
+
+        System.out.println();
+        System.out.println("Moves as map again: ");
+        System.out.println(convertMovesToMap(movesList));
     }
 }
