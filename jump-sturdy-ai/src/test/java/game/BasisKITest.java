61587ce1b66184e76c3a047a45f421f57dd3d9cf
Noel Kronenberg <79874249+noelkronenberg@users.noreply.github.com>
2024-05-18 11:34:56 +0200

    modularised tests

--- jump-sturdy-ai/src/test/java/game/BasisKITest.java
+++ jump-sturdy-ai/src/test/java/game/BasisKITest.java
@@ -16,12 +16,15 @@ public class BasisKITest {
         ki = new BasisKI();
     }
 
+    private void testMoves(String fen, String expectedMove) {
+        init();
+        assertEquals(ki.orchestrator(fen), expectedMove);
+    }
+
     @Test
     @DisplayName("Gruppe H")
     public void testGruppeH() {
-        String bestMoveKI = ki.orchestrator("1bb4/1b0b05/b01b0bb4/1b01b01b02/3r01rr2/b0r0r02rr2/4r01rr1/4r0r0 b");
-        String actualBestMove = "A6-A7";
-        assertEquals(bestMoveKI, actualBestMove);
+        testMoves("1bb4/1b0b05/b01b0bb4/1b01b01b02/3r01rr2/b0r0r02rr2/4r01rr1/4r0r0 b", "A6-A7");
     }
 
 }
