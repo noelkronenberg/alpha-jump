bceb55a0d631727aa91949092fe3a0d53ff319dd
Noel Kronenberg <79874249+noelkronenberg@users.noreply.github.com>
2024-05-27 15:45:07 +0200

    added references

--- game-server/middleware.py
+++ game-server/middleware.py
@@ -1,6 +1,6 @@
 import subprocess
 
-path = '/Users/noelkronenberg/Documents/GitHub' # NOTE: adjist to local path
+path = '/Users/noelkronenberg/Documents/GitHub' # NOTE: adjust to local path
 
 def getMove(fen, time_limit=None):
     java_class_path = f'{path}/projekt-ki/jump-sturdy-ai/target/classes'
@@ -9,7 +9,7 @@ def getMove(fen, time_limit=None):
     if time_limit is not None:
         java_command.append(str(time_limit))
     
-    process = subprocess.Popen(java_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, _ = process.communicate()
+    process = subprocess.Popen(java_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE) # reference: https://stackoverflow.com/a/92395
+    output, _ = process.communicate() # reference: https://stackoverflow.com/a/4514905
     
     return output.decode().strip()
