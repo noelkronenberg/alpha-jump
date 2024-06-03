dfd1484a33945c627038e472076ec3091943b7a1
Noel Kronenberg <79874249+noelkronenberg@users.noreply.github.com>
2024-06-03 17:22:50 +0200

    added debugger

---
+++ jump-sturdy-ai/src/main/java/debug/BasisKIDebugger.java
@@ -0,0 +1,48 @@
+package debug;
+
+import search.BasisKI;
+import game.MoveGenerator;
+import game.Color;
+
+public class BasisKIDebugger {
+
+    public static void moveOrder(String fen, String move, int sequence) {
+        BasisKI ki = new BasisKI();
+        MoveGenerator moveGenerator = new MoveGenerator();
+        moveGenerator.initializeBoard(fen);
+
+        // show board
+        String currentColor = "" + fen.charAt(fen.length() - 1);
+        System.out.println("Best move: " + move + " (" + currentColor + ")");
+
+        // move piece
+        int[] moveInt = moveGenerator.convertStringToPosWrapper(move);
+        moveGenerator.movePiece(moveInt[0], moveInt[1]);
+
+        // show board
+        System.out.println();
+        moveGenerator.printBoard(true);
+
+        for (int i = 1; i < sequence; i++)  {
+            currentColor = (currentColor.equals("r")) ? "b" : "r"; // switch color
+
+            // get best move
+            String bestMove = ki.orchestrator(moveGenerator.getFenFromBoard() + " " + currentColor, 20000.0, 0.25);
+            System.out.println("Best move: " + bestMove + " (" + currentColor + ")");
+
+            // convert move
+            int[] bestMoveInts = moveGenerator.convertStringToPosWrapper(bestMove);
+            int bestMoveInt = bestMoveInts[0] * 100 + bestMoveInts[1];
+
+            moveGenerator.movePiece(bestMoveInt); // move piece
+
+            System.out.println();
+            moveGenerator.printBoard(true); // show board
+        }
+    }
+
+    public static void main(String[] args) {
+        BasisKIDebugger.moveOrder("b0b01bb2/6b01/3bb4/4b0b02/3r04/3r04/1r0r05/1r0rrrr2 b", "D3-F4", 10);
+    }
+
+}
