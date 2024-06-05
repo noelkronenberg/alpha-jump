b1691b21f84aabbf671f6ce8ea7ba54304102551
jogoldbach <117278334+jogoldbach@users.noreply.github.com>
2024-06-05 22:09:06 +0200

    Added gson so that the python server accepts the move

--- jump-sturdy-ai/src/main/java/communication/Connection.java
+++ jump-sturdy-ai/src/main/java/communication/Connection.java
@@ -1,5 +1,7 @@
 package communication;
 
+import com.google.gson.Gson;
+
 import org.json.JSONException;
 import search.BasisKI;
 import org.json.JSONObject;
@@ -17,25 +19,36 @@ public class Connection {
         return jsonObject;
     }
 
-    public void connect(int player) {
+    public void connect() {
         this.player = player;
         BasisKI ki = new BasisKI();
         String serverAddress = "localhost";
         int port = 5555;
 
         try (Socket server = new Socket(serverAddress, port)) {
-            System.out.println("Player "+ this.player + " | " + "Connected to the server.");
 
             PrintWriter output = new PrintWriter(server.getOutputStream(), true);
 
             InputStream inputStream = server.getInputStream();
+
+            int zwischen = (int) inputStream.read();
+            if (zwischen==48){
+                this.player=1;
+            }
+            else {
+                this.player=2;
+            }
+
+            System.out.println("Player "+ this.player + " | " + "Connected to the server.");
+            
+            Gson gson = new Gson();
 
             while (true) {
                 // send request to get game status
                 output.println("\"get\"");
 
                 // wait for server response
-                Thread.currentThread().sleep(5000);
+                Thread.currentThread().sleep(500);
 
                 // get server response
                 byte[] data = new byte[9999];
@@ -72,13 +85,13 @@ public class Connection {
                         if (response.getBoolean("player1") && this.player == 1) {
                             String move = ki.orchestrator(fen, 20000.0, 0.25);
                             JSONObject moveConverted = toJSON(move);
-                            output.println(moveConverted.toString());
+                            output.println(gson.toJson(move));
                             System.out.print("\n" + "Player 1 move: " + moveConverted);
 
                         } else if (response.getBoolean("player2") && this.player == 2) {
                             String move = ki.orchestrator(fen, 20000.0, 0.25);
                             JSONObject moveConverted = toJSON(move);
-                            output.println(moveConverted.toString());
+                            output.println(gson.toJson(move));
                             System.out.println("\n" + "Player 2 move: " + moveConverted);
                         }
                     }
@@ -94,11 +107,11 @@ public class Connection {
         Connection player1 = new Connection();
         Connection player2 = new Connection();
 
-        Thread thread1 = new Thread(() -> player1.connect(1));
-        Thread.sleep(1000);
-        Thread thread2 = new Thread(() -> player2.connect(2));
+        Thread thread1 = new Thread(() -> player1.connect()); //
+        Thread thread2 = new Thread(() -> player2.connect());
 
         thread1.start();
+        Thread.sleep(100);
         thread2.start();
 
         try {
