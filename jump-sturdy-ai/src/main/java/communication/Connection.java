2795d9549b72bfbd5e0068010b747cc18984f8aa
Noel Kronenberg <79874249+noelkronenberg@users.noreply.github.com>
2024-06-06 09:58:39 +0200

    refactoring, removed continuous query

--- jump-sturdy-ai/src/main/java/communication/Connection.java
+++ jump-sturdy-ai/src/main/java/communication/Connection.java
@@ -1,56 +1,48 @@
 package communication;
 
-import com.google.gson.Gson;
-
-import org.json.JSONException;
 import search.BasisKI;
-import org.json.JSONObject;
 
+import com.google.gson.Gson;
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.io.*;
 import java.net.Socket;
 
 public class Connection {
 
     int player;
-
-    public static JSONObject toJSON(String move) {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("move", move);
-        return jsonObject;
-    }
+    String lastBoard = "";
 
     public void connect() {
-        this.player = player;
         BasisKI ki = new BasisKI();
         String serverAddress = "localhost";
         int port = 5555;
 
         try (Socket server = new Socket(serverAddress, port)) {
 
-            PrintWriter output = new PrintWriter(server.getOutputStream(), true);
-
+            PrintWriter outputStream = new PrintWriter(server.getOutputStream(), true);
             InputStream inputStream = server.getInputStream();
 
-            int zwischen = (int) inputStream.read();
-            if (zwischen==48){
-                this.player=1;
-                System.out.println("You are Player 1");
+            int temp = (int) inputStream.read();
+            if (temp == 48){
+                this.player = 1;
+                System.out.println("\n" + "You are Player 1");
             }
             else {
-                this.player=2;
-                System.out.println("You are Player 2");
+                this.player = 2;
+                System.out.println("\n" +  "You are Player 2");
             }
 
-            //System.out.println("Player "+ this.player + " | " + "Connected to the server.");
+            System.out.println("\n" + "Player "+ this.player + " | " + "Connected to the server.");
             
             Gson gson = new Gson();
 
             while (true) {
                 // send request to get game status
-                output.println("\"get\"");
+                outputStream.println("\"get\"");
 
                 // wait for server response
-                //Thread.currentThread().sleep(500);
+                Thread.sleep(500);
 
                 // get server response
                 byte[] data = new byte[9999];
@@ -60,7 +52,7 @@ public class Connection {
                 if (bytesRead != -1) {
                     // convert response
                     String jsonString;
-                    if (data[0]==49||data[0]==48){          //check whos turn it is (0 or 1)
+                    if (data[0] == 49 || data[0] == 48){ // check player turn (0 or 1)
                          jsonString = new String(data, 1, bytesRead);
                     }
                     else {
@@ -70,56 +62,62 @@ public class Connection {
                     try {
                         response = new JSONObject(jsonString);
                     } catch (JSONException e) {
-                        //System.out.println("\n" + "Player "+ this.player + " | " + "Error parsing JSON: " + jsonString);
+                        System.out.println("\n" + "Player "+ this.player + " | " + "Error parsing JSON: " + jsonString);
                         continue;
                     }
 
-                    //System.out.println("\n" + "Player "+ this.player + " | " + "Server response:  " + response);
+                    // System.out.println("\n" + "Player "+ this.player + " | " + "Server response:  " + response);
+
+                    String fen = response.getString("board");
 
                     // process server response
-                    if (response.getBoolean("bothConnected")) {
+                    if (response.getBoolean("bothConnected") && !fen.equals(this.lastBoard)) {
 
-                        String fen = response.getString("board");
-                        //System.out.println("\n" + "Player "+ this.player + " | " + "Current board: ");
-                        //System.out.println(fen);
+                        this.lastBoard = fen;
+                        System.out.println("\n" + "Player "+ this.player + " | " + "Current board: ");
+                        System.out.println(fen);
 
                         // player turns
                         if (response.getBoolean("player1") && this.player == 1) {
                             String move = ki.orchestrator(fen, 20000.0, 0.25);
-                            output.println(gson.toJson(move));
-                            System.out.print("\n" + "Player 1 move: " + move);
-
+                            outputStream.println(gson.toJson(move));
+                            System.out.print("\n" + "Player 1 | Move: " + move);
                         } else if (response.getBoolean("player2") && this.player == 2) {
                             String move = ki.orchestrator(fen, 20000.0, 0.25);
-                            output.println(gson.toJson(move));
-                            System.out.println("\n" + "Player 2 move: " + move);
+                            outputStream.println(gson.toJson(move));
+                            System.out.println("\n" + "Player 2 | Move: " + move);
                         }
                     }
                 }
             }
 
-        } catch (IOException e) {
-            System.err.println("Player "+ this.player + " | " +  "Error occurred while connecting to the server: " + e.getMessage());
+        } catch (IOException | InterruptedException e) {
+            System.err.println("\n" + "Player "+ this.player + " | " +  "Error occurred while connecting to the server: " + e.getMessage());
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
         Connection player1 = new Connection();
-        player1.connect();                              //add this
-        //Connection player2 = new Connection();          //Delete this
+        // player1.connect(false); // only for single player
 
-        //Thread thread1 = new Thread(() -> player1.connect()); //Delete this
-        //Thread thread2 = new Thread(() -> player2.connect()); //Delete this
+        // START: two player game
 
-//        thread1.start();                    //Delete this
-//        Thread.sleep(100);            //Delete this
-//        thread2.start();                    //Delete this
-//
-//        try {
-//            thread1.join();                 //Delete this
-//            thread2.join();                 //Delete this
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        Connection player2 = new Connection();
+
+        Thread thread1 = new Thread(() -> player1.connect());
+        Thread thread2 = new Thread(() -> player2.connect());
+
+        thread1.start();
+        Thread.sleep(100);
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // END: two player game
     }
 }
