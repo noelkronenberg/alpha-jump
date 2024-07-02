8566e5e464cd95dec151f8efab4b640ecec02784
Noel Kronenberg <79874249+noelkronenberg@users.noreply.github.com>
2024-07-02 20:53:11 +0200

    added JavaDoc

--- jump-sturdy-ai/src/main/java/search/KI.java
+++ jump-sturdy-ai/src/main/java/search/KI.java
@@ -1,5 +1,16 @@
 package search;
 
+/**
+ * Interface for defining an AI.
+ */
 public interface KI {
+
+    /**
+     * Orchestrates the AI on the given FEN position and search configuration.
+     *
+     * @param fen The current position in FEN notation
+     * @param config The configuration object specifying the AI search parameters
+     * @return The best move determined by the search algorithm
+     */
     String orchestrator(String fen, SearchConfig config);
 }
