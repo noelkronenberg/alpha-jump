d424d5cb842dbcca47d7492c9c0bbd9744b83516
Noel Kronenberg <79874249+noelkronenberg@users.noreply.github.com>
2024-07-03 23:59:58 +0200

    increased time limit

--- jump-sturdy-ai/src/test/java/search/Minimax_ABTest.java
+++ jump-sturdy-ai/src/test/java/search/Minimax_ABTest.java
@@ -28,7 +28,7 @@ public class Minimax_ABTest {
     public static void init() {
         ai = new Minimax_AB();
         config.timeCriterion = true;
-        config.timeLimit = 5000;
+        config.timeLimit = 10000;
         // aiMCTS = new MCTS();
     }
 
