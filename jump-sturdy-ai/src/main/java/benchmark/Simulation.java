3fce11da8ca783b2c8d94d7cca23bfd6493f92f3
Noel Kronenberg <79874249+noelkronenberg@users.noreply.github.com>
2024-07-07 18:02:43 +0200

    added benchmark runs

--- jump-sturdy-ai/src/main/java/benchmark/Simulation.java
+++ jump-sturdy-ai/src/main/java/benchmark/Simulation.java
@@ -248,19 +248,19 @@ public class Simulation {
             // configuration of first AI (CAN BE CHANGED)
             AI firstAI = new Minimax_AB();
             SearchConfig firstConfig = Minimax_AB.bestConfig.copy();
-            firstConfig.timeLimit = 3000;
+            firstConfig.timeLimit = 20000;
             firstConfig.useQuiescenceSearch = false;
 
             // configuration of second AI (CAN BE CHANGED)
             AI secondAI = new Minimax_AB();
             SearchConfig secondConfig = Minimax_AB.bestConfig.copy();
-            secondConfig.timeLimit = 3000;
+            secondConfig.timeLimit = 20000;
             secondConfig.useQuiescenceSearch = true;
             secondConfig.qSDepth = 6;
 
             // configuration of simulation (CAN BE CHANGED)
             String initialFEN = "b0b0b0b0b0b0/1b0b0b0b0b0b01/8/8/8/8/1r0r0r0r0r0r01/r0r0r0r0r0r0 b"; // sanity check: b0b0b0b0b0b0/1r0b0b0b0b0b01/8/8/8/8/1r0r0r0r0r0r01/r0r0r0r0r0r0 r (red should always win)
-            int iterations = 12;
+            int iterations = 2;
             boolean showGame = false;
 
             // start simulation (DO NOT CHANGE)
